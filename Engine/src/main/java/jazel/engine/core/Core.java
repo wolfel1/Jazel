3bc12842c6e5f2fa5743d15eed62296c931a0d5a
Wolfram Woerner <wolfram.woerner@outlook.de>
2022-08-16 17:33:06 +0200

    Add os check

--- Engine/src/main/java/jazel/engine/core/Core.java
+++ Engine/src/main/java/jazel/engine/core/Core.java
@@ -1,10 +1,25 @@
 package jazel.engine.core;
 
+import org.apache.commons.lang3.SystemUtils;
+
 public class Core {
+
+    private static boolean isDebug = false;
+    public static void checkEnvironment() {
+        isDebug = true; //TODO: maven profiles to toggle this
+
+        if (!SystemUtils.IS_OS_WINDOWS) {
+            Log.getCoreLogger().error("Currently is only Windows supported!");
+            throw new RuntimeException();
+        }
+    }
+
     public static void assertion(boolean assertion, String message) {
-        if (assertion) {
+        if (isDebug && assertion) {
             Log.getCoreLogger().error(message);
             throw new RuntimeException();
         }
     }
+
+
 }
